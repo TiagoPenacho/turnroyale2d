6a2581b4a155a9225014bd564c46b70b5be9b300
TiagoPenacho <72828411+TiagoPenacho@users.noreply.github.com>
2021-10-28 04:04:53 +0100

    Late night commit
    Collision working after 2 hours

--- TurnRoyale2D/Assets/Scripts/Player.cs
+++ TurnRoyale2D/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Player : NetworkBehaviour
 {
     public bool isLocalPlayer;
+    public float health = 10f;
     public void HandleMovement()
     {
         if (isLocalPlayer)
@@ -31,5 +32,26 @@ public class Player : NetworkBehaviour
     void Update()
     {
             HandleMovement();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // if (other.gameObject.layer == 7)
+        // {
+            GetComponent<SpriteRenderer>().color = other.gameObject.GetComponent<SpriteRenderer>().color;
+            Destroy(other.gameObject);
+            health ++;
+        // }
+        // if (other.gameObject.layer == 8)
+        // {
+        //     GetComponent<Renderer>().material.color = Color.blue;
+        //     Destroy(other.gameObject);
+        //     health ++;
+        // }
+        // if (other.gameObject.layer == 9)
+        // {
+        //     GetComponent<Renderer>().material.color = Color.yellow;
+        //     Destroy(other.gameObject);
+        //     health ++;
+        // }
     }
 }
