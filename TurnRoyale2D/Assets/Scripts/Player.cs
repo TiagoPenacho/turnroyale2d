81728f6bf96e88c45d0d44a27610610eb23dd2d9
TiagoPenacho <72828411+TiagoPenacho@users.noreply.github.com>
2021-10-28 02:49:23 +0100

    Day 1
    Added Network
    Need to add random spawn pips
    Need to add path finding

--- TurnRoyale2D/Assets/Scripts/Player.cs
+++ TurnRoyale2D/Assets/Scripts/Player.cs
@@ -1,18 +1,35 @@
+using Unity.Netcode;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Player : MonoBehaviour
+using UnityEngine.UI;
+public class Player : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isLocalPlayer;
+    public void HandleMovement()
     {
-        
+        if (isLocalPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.Translate(new Vector2(1f,0f));
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.Translate(new Vector2(-1f,0f));
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.Translate(new Vector2(0f,1f));
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.Translate(new Vector2(0f,-1f));
+            }
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+            HandleMovement();
     }
 }
